7a5b4a09e4b4eda1700e4f85e88ac5bb7a23953b
Nelson <gmz_nlsn@yahoo.com>
2022-09-06 16:46:45 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const writeCards = (arr, event) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+}
+
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise'));
+
+const countDown = (num) => {
+
+    while (num >= 0) {
+
+        console.log(num--);
+    }
+    
+}
+
+console.log(countDown(10));
